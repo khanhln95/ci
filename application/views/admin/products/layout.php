53cde8adc5258e095c27e79207def0d12c1302e2
khanh <khanhln22121995@gmail.com>
2018-01-29 11:28:47 +0700

    fix

--- application/views/admin/products/layout.php
+++ application/views/admin/products/layout.php
@@ -211,84 +211,6 @@
 									<a href=""><span class="fa fa-heart-o"></span></a>
 									<a href=""><span class="fa fa-exchange"></span></a>
 								</div>
-								<img src="<?=base_url()?>assets/web/img/cloting.jpg">
-
-
-							</div>
-							<div class="name">
-								Eleven Paris T-Shirt with
-							</div>
-							<div class="cost">
-								<p>$696,000 <span>$700.000</span></p>
-							</div>
-							<div class='rating-widget'>
-								<fieldset class="rating">
-									<input type="radio" id="star5" name="rating" value="5" /><label class="full" for="star5" title="Awesome - 5 stars"></label>
-									<input type="radio" id="star4half" name="rating" value="4 and a half" /><label class="half" for="star4half" title="Pretty good - 4.5 stars"></label>
-									<input type="radio" id="star4" name="rating" value="4" /><label class="full" for="star4" title="Pretty good - 4 stars"></label>
-									<input type="radio" id="star3half" name="rating" value="3 and a half" /><label class="half" for="star3half" title="Meh - 3.5 stars"></label>
-									<input type="radio" id="star3" name="rating" value="3" /><label class="full" for="star3" title="Meh - 3 stars"></label>
-									<input type="radio" id="star2half" name="rating" value="2 and a half" /><label class="half" for="star2half" title="Kinda bad - 2.5 stars"></label>
-									<input type="radio" id="star2" name="rating" value="2" /><label class="full" for="star2" title="Kinda bad - 2 stars"></label>
-									<input type="radio" id="star1half" name="rating" value="1 and a half" /><label class="half" for="star1half" title="Meh - 1.5 stars"></label>
-									<input type="radio" id="star1" name="rating" value="1" /><label class="full" for="star1" title="Sucks big time - 1 star"></label>
-									<input type="radio" id="starhalf" name="rating" value="half" /><label class="half" for="starhalf" title="Sucks big time - 0.5 stars"></label>
-								</fieldset>
-							</div>
-						</div>
-						<div class="item">
-							<div class="img-sp">
-								<div class="saler">
-									<a href="#" class="new">New</a>
-									<br>
-									<a href="#" class="sale">Sale</a>
-									<p class="rate">-15%</p>
-								</div>
-
-								<div class="hover">
-									<a href=""><span class="glyphicon glyphicon-shopping-cart"></span></a>
-									<a href=""><span class="fa fa-heart-o"></span></a>
-									<a href=""><span class="fa fa-exchange"></span></a>
-								</div>
-								<img src="<?=base_url()?>assets/web/img/t-shirt.jpg">
-
-
-							</div>
-							<div class="name">
-								Knight T-Shirt in Cotton
-							</div>
-							<div class="cost">
-								<p>$696,000 <span>$700.000</span></p>
-							</div>
-							<div class='rating-widget'>
-								<fieldset class="rating">
-									<input type="radio" id="star5" name="rating" value="5" /><label class="full" for="star5" title="Awesome - 5 stars"></label>
-									<input type="radio" id="star4half" name="rating" value="4 and a half" /><label class="half" for="star4half" title="Pretty good - 4.5 stars"></label>
-									<input type="radio" id="star4" name="rating" value="4" /><label class="full" for="star4" title="Pretty good - 4 stars"></label>
-									<input type="radio" id="star3half" name="rating" value="3 and a half" /><label class="half" for="star3half" title="Meh - 3.5 stars"></label>
-									<input type="radio" id="star3" name="rating" value="3" /><label class="full" for="star3" title="Meh - 3 stars"></label>
-									<input type="radio" id="star2half" name="rating" value="2 and a half" /><label class="half" for="star2half" title="Kinda bad - 2.5 stars"></label>
-									<input type="radio" id="star2" name="rating" value="2" /><label class="full" for="star2" title="Kinda bad - 2 stars"></label>
-									<input type="radio" id="star1half" name="rating" value="1 and a half" /><label class="half" for="star1half" title="Meh - 1.5 stars"></label>
-									<input type="radio" id="star1" name="rating" value="1" /><label class="full" for="star1" title="Sucks big time - 1 star"></label>
-									<input type="radio" id="starhalf" name="rating" value="half" /><label class="half" for="starhalf" title="Sucks big time - 0.5 stars"></label>
-								</fieldset>
-							</div>
-						</div>
-						<div class="item">
-							<div class="img-sp">
-								<div class="saler">
-									<a href="#" class="new">New</a>
-									<br>
-									<a href="#" class="sale">Sale</a>
-									<p class="rate">-15%</p>
-								</div>
-
-								<div class="hover">
-									<a href=""><span class="glyphicon glyphicon-shopping-cart"></span></a>
-									<a href=""><span class="fa fa-heart-o"></span></a>
-									<a href=""><span class="fa fa-exchange"></span></a>
-								</div>
 								<?php foreach ($result as $i => $item) { ?>
 								<?php if($item->picture != ''){?>
 								<img src="<?=base_url()?>uploads/<?=$item->picture?>">
@@ -300,10 +222,10 @@
 
 							</div>
 							<div class="name">
-								Knight T-Shirt in Cotton
+								<?php echo $item->name ?>
 							</div>
 							<div class="cost">
-								<p>$696,000 <span>$700.000</span></p>
+								<p>$<?php echo $item->price ?> <span>$700.000</span></p>
 							</div>
 							<div class='rating-widget'>
 								<fieldset class="rating">
