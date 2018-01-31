a445c380ab3048870108854a14a92a9cff298e2c
khanh <khanhln22121995@gmail.com>
2018-01-31 15:20:40 +0700

    fff

--- application/views/web/index.php
+++ application/views/web/index.php
@@ -4,7 +4,12 @@
 	</head>
 	<body>
 		<?php $this->load->view('web/page/header')?>
-		<?php $this->load->view('web/page/content')?>
+		<?php $this->load->view('web/page/clothing')?>
+		<?php $this->load->view('web/page/bag_shoe')?>
+		<?php $this->load->view('web/page/sale')?>
+		<?php $this->load->view('web/page/blog')?>
+		<?php $this->load->view('web/page/about')?>
+		<?php $this->load->view('web/page/advertise')?>
 		<?php $this->load->view('web/page/footer')?>
 	</body>
 </html>
